a86eab2e9b7893b1c8986d4d9ec91cbc6bfb7957
Edney O. S. Filho <edney.osf@gmail.com>
2021-04-29 00:25:49 -0300

    Movimentaçao do mouse com x11

--- mokeyed.c
+++ mokeyed.c
@@ -7,8 +7,8 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
-int main(int argc, char *argv[]){
-  
+void moveMyMouse(int x, int y){
+ 
   Display *dpy;
   Window root_window;
 
@@ -16,11 +16,23 @@ int main(int argc, char *argv[]){
 
   root_window = XRootWindow(dpy, 0);
   XSelectInput(dpy, root_window, KeyReleaseMask);
-  XWarpPointer(dpy, None, root_window, 0, 0, 0, 0, 180, 500);
+  XWarpPointer(dpy, None, root_window, 0, 0, 0, 0, x, y);
 
   XGrabPointer(dpy, root_window, True, ButtonPressMask, GrabModeAsync,GrabModeAsync, None, None, CurrentTime);
 
   XFlush(dpy);
+
+  return;
+}
+
+int main(int argc, char *argv[]){
+ 
+  for(int i = 0 ; i < 500 ; i++){ 
+  
+    moveMyMouse(180+i, 500+i);
+    usleep(1000);
+
+  }
 
   return 0;
 }
